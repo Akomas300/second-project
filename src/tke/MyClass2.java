17a8143e7a08a8bfc1150d984774573951bd8b10
akowa <akowa344@gmail.com>
2024-07-09 16:46:30 -0400

    some changes

--- src/tke/MyClass2.java
+++ src/tke/MyClass2.java
@@ -3,5 +3,6 @@ package tke;
 public class MyClass2 {
     public static void main(String[] args) {
         System.out.println("this is my test class");
+        System.out.println("it is not working");
     }
 }
